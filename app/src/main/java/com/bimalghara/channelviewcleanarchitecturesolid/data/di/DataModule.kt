fce67a6e06cd20bda564e019d8379c1826d37561
Bimal Ghara <bimal.ghara@secomind.ai>
2023-01-28 15:59:06 +0530

    database and LocalDataSource added in DI (data layer)

--- app/src/main/java/com/bimalghara/channelviewcleanarchitecturesolid/data/di/DataModule.kt
+++ app/src/main/java/com/bimalghara/channelviewcleanarchitecturesolid/data/di/DataModule.kt
@@ -1,11 +1,17 @@
 package com.bimalghara.channelviewcleanarchitecturesolid.data.di
 
+import android.app.Application
+import androidx.room.Room
 import com.bimalghara.channelviewcleanarchitecturesolid.data.error.mapper.ErrorMapperImpl
 import com.bimalghara.channelviewcleanarchitecturesolid.data.error.mapper.ErrorMapperSource
+import com.bimalghara.channelviewcleanarchitecturesolid.data.local.LocalDataImpl
+import com.bimalghara.channelviewcleanarchitecturesolid.data.local.LocalDataSource
+import com.bimalghara.channelviewcleanarchitecturesolid.data.local.database.AppDatabase
 import com.bimalghara.channelviewcleanarchitecturesolid.data.repository.ErrorDetailsImpl
 import com.bimalghara.channelviewcleanarchitecturesolid.domain.repository.ErrorDetailsSource
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -28,3 +34,30 @@ abstract class DataModuleErrors {
 
 }
 
+
+
+
+
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DataModuleDataSources {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application): AppDatabase {
+        return Room.databaseBuilder(
+            app,
+            AppDatabase::class.java,
+            AppDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocalData(db: AppDatabase): LocalDataSource {
+        return LocalDataImpl(db.episodesDao)
+    }
+
+}
