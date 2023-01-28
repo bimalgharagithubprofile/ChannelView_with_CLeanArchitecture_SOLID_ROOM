ea17cdbc6aa7c5f5ac413e7e9369cc8c00b3381c
Bimal Ghara <bimal.ghara@secomind.ai>
2023-01-28 12:16:34 +0530

    view related extension fun added

---
+++ app/src/main/java/com/bimalghara/channelviewcleanarchitecturesolid/utils/ViewExt.kt
@@ -0,0 +1,78 @@
+package com.bimalghara.channelviewcleanarchitecturesolid.utils
+
+import android.content.Context
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.DrawableRes
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.bimalghara.channelviewcleanarchitecturesolid.R
+import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
+
+/**
+ * Created by BimalGhara
+ */
+
+
+
+/**
+ * switch between visibility of view
+*/
+fun View.toVisible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.toGone() {
+    this.visibility = View.GONE
+}
+
+fun View.toInvisible() {
+    this.visibility = View.GONE
+}
+
+
+/**
+ * Transforms static java function SnackBar.make() to an extension function on View.
+ */
+fun View.showSnackBar(snackBarText: String, timeLength: Int) {
+    Snackbar.make(this, snackBarText, timeLength).run {
+        addCallback(object : Snackbar.Callback() {
+            override fun onShown(sb: Snackbar?) {
+            }
+            override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+            }
+        })
+        show()
+    }
+}
+
+
+fun Context.toast(message: String){
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun View.showToast(
+    lifecycleOwner: LifecycleOwner,
+    toastEvent: LiveData<SingleEvent<Any>>,
+    timeLength: Int
+) {
+
+    toastEvent.observe(lifecycleOwner, Observer { event ->
+        event.getContentIfNotHandled()?.let {
+            when (it) {
+                is String -> Toast.makeText(this.context, it, timeLength).show()
+                is Int -> Toast.makeText(this.context, this.context.getString(it), timeLength).show()
+                else -> {
+                }
+            }
+        }
+    })
+}
+
+
+fun ImageView.loadImage(@DrawableRes resId: Int) = Picasso.get().load(resId).into(this)
+fun ImageView.loadImage(url: String) = Picasso.get().load(url).placeholder(R.drawable.ic_logo_foreground).error(R.drawable.ic_logo_foreground).into(this)
+
