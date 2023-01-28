47db5ab494a64cad9d5e4944daa72a182d2438ec
Bimal Ghara <bimal.ghara@secomind.ai>
2023-01-28 12:18:08 +0530

    hilt initiated and splash added

---
+++ app/src/main/java/com/bimalghara/channelviewcleanarchitecturesolid/BaseApplication.kt
@@ -0,0 +1,11 @@
+package com.bimalghara.channelviewcleanarchitecturesolid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by BimalGhara
+ */
+
+@HiltAndroidApp
+class BaseApplication : Application()
---
+++ app/src/main/java/com/bimalghara/channelviewcleanarchitecturesolid/presentation/splash/SplashActivity.kt
@@ -0,0 +1,44 @@
+package com.bimalghara.channelviewcleanarchitecturesolid.presentation.splash
+
+import android.content.Intent
+import android.os.Bundle
+import com.bimalghara.channelviewcleanarchitecturesolid.databinding.ActivitySplashBinding
+import com.bimalghara.channelviewcleanarchitecturesolid.presentation.MainActivity
+import com.bimalghara.channelviewcleanarchitecturesolid.presentation.base.BaseActivity
+import com.bimalghara.channelviewcleanarchitecturesolid.utils.SPLASH_DELAY
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * Created by BimalGhara
+ */
+
+@AndroidEntryPoint
+class SplashActivity : BaseActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun initViewBinding() {
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        navigateToMainScreen()
+    }
+
+
+    private fun navigateToMainScreen() {
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(SPLASH_DELAY.toLong())
+
+            val nextScreenIntent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(nextScreenIntent)
+            finish()
+        }
+    }
+}
