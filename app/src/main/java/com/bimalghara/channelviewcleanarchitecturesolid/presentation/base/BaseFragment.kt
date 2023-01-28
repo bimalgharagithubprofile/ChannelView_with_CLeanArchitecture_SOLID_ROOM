c90fceb0797891afe0aa2e68512e9af791ab25a0
Bimal Ghara <bimal.ghara@secomind.ai>
2023-01-28 12:48:00 +0530

    few helpers added for error string and handling single observe livedata event and constants

---
+++ app/src/main/java/com/bimalghara/channelviewcleanarchitecturesolid/presentation/base/BaseFragment.kt
@@ -0,0 +1,43 @@
+package com.bimalghara.channelviewcleanarchitecturesolid.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.viewbinding.ViewBinding
+import com.bimalghara.channelviewcleanarchitecturesolid.utils.SingleEvent
+import com.bimalghara.channelviewcleanarchitecturesolid.utils.showToast
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * Created by BimalGhara
+ */
+
+abstract class BaseFragment<B: ViewBinding> : Fragment() {
+    protected lateinit var binding: B
+    abstract fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): B
+
+    abstract fun observeViewModel()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = getFragmentBinding(inflater, container)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeViewModel()
+    }
+
+    fun observeError(event: LiveData<SingleEvent<Any>>) {
+        binding.root.showToast(this, event, Snackbar.LENGTH_LONG)
+    }
+}
